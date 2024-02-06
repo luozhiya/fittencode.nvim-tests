7f0013b7a200a16e9f9446c48d637f49ff979f2f
luozhiya <luozhiya@petalmail.com>
2024-02-06 18:49:30 +0800

    1.cc 2.cc

---
+++ cc/2.cc
@@ -0,0 +1,11 @@
+// KMP algorithm for pattern matching
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+int main() {
+    string s = "ABABDABACDABABCABAB";
+    string p = "ABABCABAB";
+
+
